3bc7dd9c33416d28a46c7d3d3971c6537a0f2a8d
cosintsev228@gmail.com <cositsev228@gmail.com>
2021-04-27 01:01:50 +0300

    3 zsdanie

--- testtest.php
+++ testtest.php
@@ -1,9 +1,10 @@
 <?php
 
-$db = pg_connect('host=localhost dbname=aboba user=postgres password=fji470ki');
+$db = pg_connect('host=localhost dbname=test_bd user=postgres password=mn124560MN');
 
 $db_title = $_POST['title'];
 $db_content = $_POST['content'];
 $db_date = $_POST['date'];
 
 pg_query($db,"INSERT INTO posts (title, content, date) VALUES ($db_title, $db_content, $db_date)");
+
